a3bacf8339b1fa91701a0b2660acd040fb59b976
Donald Angelillo <dangelillo@gmail.com>
2017-08-24 17:38:12 -0400

    initial commit

---
+++ HHiTunesRSSFeedChecker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HHiTunesRSSFeedChecker
+//
+//  Created by Donald Angelillo on 8/24/17.
+//  Copyright © 2017 Donald Angelillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target 'HHiTunesRSSFeedChecker' do
+    pod 'Alamofire'
+end
+
+
